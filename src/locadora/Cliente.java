c2a0d7e9220dc39b5b39a9df6a46c7119e851764
Felipejjjj <felipe.raimundo@academico.ifpb.edu.br>
2026-03-26 15:36:40 -0300

    Lista 1.2.3 Grasp-Refactoring-rentcars

--- src/locadora/Cliente.java
+++ src/locadora/Cliente.java
@@ -21,6 +21,31 @@ public class Cliente {
 		carrosAlugados.add(locacao);
 	}
 
+
+	public double getValorTotal(){
+		double total = 0.0;
+
+		Iterator<Locacao> locacoes = carrosAlugados.iterator();
+
+		while(locacoes.hasNext()) {
+			 Locacao cada = locacoes.next();
+			 total += cada.valorDeUmaLocacao();
+	}
+	return total;
+}
+
+	public int getPontosTotaisDeLocadorFrequente(){
+		int pontos = 0;
+
+		Iterator<Locacao> locacoes = carrosAlugados.iterator();
+		while(locacoes.hasNext()) {
+			 Locacao cada = locacoes.next();
+			 pontos += cada.calcularPontos();
+
+	}
+	return pontos;
+}
+
 	private String gerarCabecalhoExtrato() {
 		String resultado = "Registro de Locações de " + getNome() + fimDeLinha;
 		resultado += String.format("Seq Automovel             Ano  Diarias   Valor Pago"+fimDeLinha);
@@ -28,42 +53,7 @@ public class Cliente {
 		return resultado;
 	}
 
-	private double valorDeUmaLocacao(Locacao locacao){
-		double valor = 0.0;
-
-			// determina valores para cada linha
-			switch(locacao.getCarro().getCodigoDoPreco()) {
-			case Automovel.BASICO: // R$ 90.00 por dia
-				valor += locacao.getDiasAlugado() * 90.0;
-				break;
-
-			case Automovel.FAMILIA: // R$ 130.00 por dia
-				valor += locacao.getDiasAlugado() * 130.0;
-				break;
-
-			case Automovel.LUXO: // R$ 200.00 por dia
-				valor += locacao.getDiasAlugado() * 200.0;
-				
-				// Adiciona um desconto de 10% se alugar o carro por mais de 4 dias
-				if(locacao.getDiasAlugado() > 4) {
-					valor *= 0.9;
-				}
-				break;
-			}
-		return valor;
-			
-	}
 	
-	private int calcularPontos(Locacao locacao) {
-    int pontos = 1;
-
-    if(locacao.getCarro().getCodigoDoPreco() == Automovel.LUXO &&
-       locacao.getDiasAlugado() > 2) {
-        pontos += 2;
-    }
-
-    return pontos;
-	}
 
 	private String montarLinhaExtrato(int sequencia, Locacao locacao, double valor) {
     return String.format("%02d. %-20s  %4d    %2d     R$ %8.2f" + fimDeLinha,
@@ -83,31 +73,22 @@ public class Cliente {
 
 	public String extrato() {
 
-		double valorTotal = 0.0;
-		int pontosDeLocadorFrequente = 0;
+	
 		int sequencia = 0;
 	
 		String resultado = gerarCabecalhoExtrato();
-
-
 		Iterator<Locacao> locacoes = carrosAlugados.iterator();
 
-		while(locacoes.hasNext()) {
-			 Locacao cada = locacoes.next();
-
-        double valorCorrente = valorDeUmaLocacao(cada);
-
-        pontosDeLocadorFrequente += calcularPontos(cada);
+    	while(locacoes.hasNext()) {
+        Locacao cada = locacoes.next();
 
         sequencia++;
-
-        resultado += montarLinhaExtrato(sequencia, cada, valorCorrente);
-
-        valorTotal += valorCorrente;
+		
+        
+        resultado += montarLinhaExtrato(sequencia, cada, cada.valorDeUmaLocacao());
     }
-
-    resultado += gerarRodapeExtrato(valorTotal, pontosDeLocadorFrequente);
-
-    return resultado;
+	
+	resultado += gerarRodapeExtrato(getValorTotal(), getPontosTotaisDeLocadorFrequente());
+	return resultado;
 }
 }
--- src/locadora/Locacao.java
+++ src/locadora/Locacao.java
@@ -16,4 +16,42 @@ public class Locacao {
 	   public int getDiasAlugado() {
 	      return diasAlugado;
 	   }
+
+	   public double valorDeUmaLocacao(){
+		double valor = 0.0;
+
+			// determina valores para cada linha
+			switch(this.getCarro().getCodigoDoPreco()) {
+			case Automovel.BASICO: // R$ 90.00 por dia
+				valor += this.getDiasAlugado() * 90.0;
+				break;
+
+			case Automovel.FAMILIA: // R$ 130.00 por dia
+				valor += this.getDiasAlugado() * 130.0;
+				break;
+
+			case Automovel.LUXO: // R$ 200.00 por dia
+				valor += this.getDiasAlugado() * 200.0;
+				
+				// Adiciona um desconto de 10% se alugar o carro por mais de 4 dias
+				if(this.getDiasAlugado() > 4) {
+					valor *= 0.9;
+				}
+				break;
+			}
+		return valor;
+			
+	}
+
+	public int calcularPontos() {
+    int pontos = 1;
+
+    if(getCarro().getCodigoDoPreco() == Automovel.LUXO &&
+       getDiasAlugado() > 2) {
+        pontos += 2;
+    }
+
+    return pontos;
+	}
+	
 }
